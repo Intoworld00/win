60b7736eedd0c4e2ec04b0f0ce59098e957e6919
Intoworld00 <158583502+Intoworld00@users.noreply.github.com>
2024-02-02 17:05:42 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# usr/bin/github/__init__.py
+################################
+# Welcome to github attribute  #
+################################
+import tkinter as tk
+from tkinter import *
+main = tk.Tk()
+input("Do you want to create a turtle main ? (Y/N): ");
