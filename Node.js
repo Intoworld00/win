dae8b75a1d72e953868a9d0113b96065be48fcd9
Intoworld00 <158583502+Intoworld00@users.noreply.github.com>
2024-02-05 18:32:40 +0100

    Main of win

---
+++ Node.js
@@ -0,0 +1,11 @@
+function main(params)
+{
+  var params = 2
+  var Yd62Hack = main.argsMain()
+  var params600 = true;
+  function cutOutsideJohnPickData(params600)
+    {
+        var data2 = 0x652441800734309,0x35322442524222401342,0x7262436171830;
+        cutOutsideJohnPickData.data("https://<thewebsiteyouwant>.<ext>/<optional_next_of_web>", 0x<data>, 0x<data2>)
+    }
+}
